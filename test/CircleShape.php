73f8693d08b1cde8a527c41a80e0c7e6e340cad6
Nuwan Chamara <nuwanchamramail@gmail.com>
2017-09-08 13:49:37 +0530

    circleShape crop

---
+++ test/CircleShape.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nuwan
+ * Date: 08/09/2017
+ * Time: 10:44
+ */
+
+namespace test;
+//namespace App\test\circle;
+
+use App\lib\Image;
+
+class CircleShape extends Image
+{
+
+
+    private $src_img;
+    private $src_w;
+    private $src_h;
+    private $dst_img;
+    private $dst_w;
+    private $dst_h;
+
+
+    /**
+     * CircleShape constructor.
+     */
+    public function __construct($image)
+    {
+        parent::__construct($image);
+
+        $this->src_img = $this->getResource();
+        $this->src_w = $this->getWidth();
+        $this->src_h = $this->getHeight();
+        $this->dst_w = $this->getWidth();
+        $this->dst_h = $this->getHeight();
+
+    }
+
+
+
+    public function build()
+    {
+        $this->crop();
+    }
+
+
+
+
+    public function __destruct()
+    {
+        if (is_resource($this->getResource()))
+        {
+            imagedestroy($this->getResource());
+        }
+    }
+
+    public function display()
+    {
+        header("Content-type: image/png");
+        imagepng($this->getResource());
+        return $this;
+    }
+
+    public function reset()
+    {
+        if (is_resource(($this->dst_img)))
+        {
+            imagedestroy($this->dst_img);
+        }
+        $this->dst_img = imagecreatetruecolor($this->dst_w, $this->dst_h);
+        imagecopy($this->dst_img, $this->src_img, 0, 0, 0, 0, $this->dst_w, $this->dst_h);
+        return $this;
+    }
+
+    public function size($dstWidth, $dstHeight)
+    {
+        $this->dst_w = $dstWidth;
+        $this->dst_h = $dstHeight;
+        return $this->reset();
+    }
+
+    public function crop()
+    {
+        // Intializes destination image
+        $this->reset();
+
+        // Create a black image with a transparent ellipse, and merge with destination
+        $mask = imagecreatetruecolor($this->dst_w, $this->dst_h);
+        $maskTransparent = imagecolorallocate($mask, 255, 0, 255);
+        imagecolortransparent($mask, $maskTransparent);
+        imagefilledellipse($mask, $this->dst_w / 2, $this->dst_h / 2, $this->dst_w, $this->dst_h, $maskTransparent);
+        imagecopymerge($this->dst_img, $mask, 0, 0, 0, 0, $this->dst_w, $this->dst_h, 100);
+
+        // Fill each corners of destination image with transparency
+        $dstTransparent = imagecolorallocate($this->dst_img, 255, 0, 255);
+        imagefill($this->dst_img, 0, 0, $dstTransparent);
+        imagefill($this->dst_img, $this->dst_w - 1, 0, $dstTransparent);
+        imagefill($this->dst_img, 0, $this->dst_h - 1, $dstTransparent);
+        imagefill($this->dst_img, $this->dst_w - 1, $this->dst_h - 1, $dstTransparent);
+        imagecolortransparent($this->dst_img, $dstTransparent);
+        $this->setResource($this->dst_img);
+        return $this;
+    }
+
+}
